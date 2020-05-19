3fad2306a431956401436982fea4cdd51e32d516
elucserr <elenalucasserrano@gmail.com>
2020-05-19 20:20:04 +0200

    added in env url frontend

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ const app = express();
 app.use(
 	cors({
 		credentials: true,
-		origin: [process.env.FRONTEND_DOMAIN],
+		origin: [process.env.FRONTEND_DOMAIN, process.env.FRONTEND_DEPLOY],
 	})
 );
 app.use(logger('dev'));
