ba4bb131a1b69e792bef62df24525871ef3267e0
elucserr <elenalucasserrano@gmail.com>
2020-05-23 12:50:39 +0200

    no email

--- models/User.js
+++ models/User.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
 	{
 		username: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
-		email: { type: String, unique: true },
+		//email: { type: String, unique: true },
 		dateOfBirth: String,
 		typeOfDiabetes: {
 			type: String,
